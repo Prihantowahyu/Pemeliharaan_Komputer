46775957e4e5ab21cf2e47be81a7396edb9620ef
Wahyu Prihanto <prihanto.wahyu@gmail.com>
2026-08-31 11:50:00 +0700

    Comprehensive upgrade: Full 9 JP (405 mins) pedagogical lesson plans, deep technical guides, job sheets, troubleshooting labs & CBT for all 20 Server modules (Ganjil & Genap XII)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'bengkel-hw-pwa-v22-genap-xii-complete';
+const CACHE_NAME = 'bengkel-hw-pwa-v23-infrastruktur-9jp-complete';
 
 // DAFTAR LENGKAP SEMUA FILE UNTUK AKSES FULL OFFLINE
 const ASSETS_TO_CACHE = [
